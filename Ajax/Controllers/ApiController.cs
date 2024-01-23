6724a2605c306bfd594a7ffea35b948dee31401c
WHC3 <jim3090666@gmail.com>
2024-01-23 09:24:20 +0800

    加入專案檔案。

---
+++ Ajax/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using Ajax.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ajax.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+        public ApiController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        } 
+        public IActionResult Cities()
+        {
+            var  cities = _dbContext.Addresses.Select(x => x.City).Distinct(); 
+            return Json(cities); 
+        }
+    }
+}
---
+++ Ajax/Controllers/HomeWorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ajax.Controllers
+{
+    public class HomeWorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
